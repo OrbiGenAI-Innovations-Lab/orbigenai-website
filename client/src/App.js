28c76308c9f927fe68bd48fe2d80dcc6a6127526
Tanya <tanyakemkar@gmail.com>
2023-11-15 18:38:27 +0530

    coming soon

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='font-bold m-5 text-[#0D7AFB]'>
+        <div>OrbiGenAI</div>
+        <div>Innovations Lab</div>
+      </div>
+      <div className='text-center lg:mt-28 sm:mt-16 align-middle	'>
+        <div className='lg:text-2xl sm:text-lg'>YOUR FUTURE POWERED BY OUR AI</div>
+        <div className='font-extrabold lg:text-8xl text-3xl'>Transforming <br/> Research with AI</div>
+        <div className='lg:text-xl m-5 lg:mx-52 sm:mx-5 text-base'>Our team of brilliant minds is hard at work, crafting the website. Stay tuned as we embark on this 
+          epic journey. Brace yourself for a wave of innovation that will transform the way we live, work, and
+          interact with technology. We're not just dreaming of a better future; we're building it.
+        </div>
+        <div className='lg:text-xl text-base'>The Future is bright. The Future is <span className='font-bold'>OrbiGenAI Innovations Lab.</span></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
